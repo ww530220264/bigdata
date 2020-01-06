3f47295847c71b4642e2022385f0bbde9ab519a1
ww530220264 <530220264@qq.com>
2020-01-06 23:10:14 +0800

    增加pageView点击数据生成器 增加pageViewStream

---
+++ src/main/scala/org/ww/spark/streaming/clickstream/PageViewGenerator.scala
@@ -0,0 +1,82 @@
+package org.ww.spark.streaming.clickstream
+
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+import scala.util.Random
+
+class PageView(val url: String, val status: Int,
+               val zipCode: Int, val userID: Int) extends Serializable {
+  override def toString: String = {
+    "%s\t%s\t%s\t%s\n".format(url, status, zipCode, userID)
+  }
+}
+
+object PageView extends Serializable {
+  def fromString(in: String): PageView = {
+    val parts = in.split("\t")
+    new PageView(parts(0), parts(1).toInt, parts(2).toInt, parts(3).toInt)
+  }
+}
+
+object PageViewGenerator {
+  val pages = Map("http://foo.com/" -> .7,
+    "http://foo.com/news" -> 0.2,
+    "http://foo.com/contact" -> .1)
+  val httpStatus = Map(200 -> .95,
+    404 -> .05)
+  val userZipCode = Map(94709 -> .5,
+    94117 -> .5)
+  val userID = Map((1 to 100).map(_ -> .01): _*)
+
+  def pickFromDistribution[T](inputMap: Map[T, Double]): T = {
+    val rand = new Random().nextDouble()
+    var total = 0.0
+    for ((item, prob) <- inputMap) {
+      total = total + prob
+      if (total > rand) {
+        return item
+      }
+    }
+    inputMap.take(1).head._1
+  }
+
+  def getNextClickEvent(): String = {
+    val id = pickFromDistribution(userID)
+    val page = pickFromDistribution(pages)
+    val status = pickFromDistribution(httpStatus)
+    val zipCode = pickFromDistribution(userZipCode)
+    new PageView(page, status, zipCode, id).toString
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 2) {
+      System.err.println("Usage: PageViewGenerator <port> <viewsPerSecond>")
+      System.exit(1)
+    }
+    val port = args(0).toInt
+    val viewsPerSecond = args(1).toInt
+    //    val sleepDelayMs = (1000.0 / viewsPerSecond).toInt
+    val sleepDelayMs = viewsPerSecond
+    val listener = new ServerSocket(port)
+    println(s"Listening on port: $port")
+
+    while (true) {
+      val socket = listener.accept()
+      new Thread() {
+        override def run(): Unit = {
+          println(s"Got client connected from ${socket.getInetAddress}")
+          val out = new PrintWriter(socket.getOutputStream, true)
+
+          while (true) {
+            Thread.sleep(sleepDelayMs)
+            out.write(getNextClickEvent())
+            out.flush()
+          }
+          socket.close()
+        }
+      }.start()
+    }
+  }
+
+}
---
+++ src/main/scala/org/ww/spark/streaming/clickstream/PageViewStream.scala
@@ -0,0 +1,68 @@
+package org.ww.spark.streaming.clickstream
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object PageViewStream {
+  def main(args: Array[String]): Unit = {
+    if (args.length != 3) {
+      System.err.println("Usage: PageViewStream <metric> <host> <port>")
+      System.err.println("<metric> must be one of pageCounts, slidingPageCounts," +
+        " errorRatePerZipCode, activeUserCount, popularUsersSeen")
+      System.exit(1)
+    }
+    val metric = args(0)
+    val host = args(1)
+    val port = args(2).toInt
+
+    val sparkConf = new SparkConf().setAppName("PageViewStream").setMaster("local[2]")
+    //    val ssc = new StreamingContext("local[2]",
+    //      "PageViewStream", Seconds(1),
+    //      System.getenv("SPARK_HOME"),
+    //      StreamingContext.jarOfClass(this.getClass).toSeq)
+    val ssc = new StreamingContext(sparkConf, Seconds(1))
+
+    val pageViews = ssc.socketTextStream(host, port).flatMap(_.split("\n")).map(PageView.fromString(_))
+    val pageCounts = pageViews.map(view => view.url).countByValue()
+    val slidingPageCounts = pageViews.map(view => view.url)
+      .countByValueAndWindow(Seconds(10), Seconds(2))
+    val statusesPerZipCode = pageViews.window(Seconds(30), Seconds(2))
+      .map(view => ((view.zipCode, view.status))).groupByKey()
+    val errorRatePerZipCode = statusesPerZipCode.map {
+      case (zip, statuses) =>
+        val normalCount = statuses.count(_ == 200)
+        val errorCount = statuses.size - normalCount
+        val errorRatio = errorCount.toFloat / statuses.size
+        if (errorRatio > 0.05) {
+          "%s: **%s**".format(zip, errorRatio)
+        } else {
+          "%s: %s".format(zip, errorRatio)
+        }
+    }
+    val activeUserCount = pageViews.window(Seconds(15), Seconds(2))
+      .map(view => (view.userID, 1))
+      .groupByKey()
+      .count()
+      .map("Unique active users" + _)
+    val userList = ssc.sparkContext.parallelize(Seq(
+      1 -> "Patrick Wendell",
+      2 -> "Reynold Xin",
+      3 -> "Matei Zahariz"
+    ))
+    metric match {
+      case "pageCounts" => pageCounts.print()
+      case "slidingPageCounts" => slidingPageCounts.print()
+      case "errorRatePerZipCode" => errorRatePerZipCode.print()
+      case "activeUserCount" => activeUserCount.print()
+      case "popularUsersSeen" =>
+        pageViews.map(view => (view.userID, 1))
+          .foreachRDD((rdd, time) => rdd.join(userList).map(_._2._2).take(10)
+            .foreach(u => println(s"Saw user $u at time $time")))
+      case _ => println(s"Invalid metric entered: $metric")
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
