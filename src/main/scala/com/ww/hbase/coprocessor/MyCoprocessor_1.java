4cae9798d64af2fb913cf728e826a557af2edefc
ww530220264 <530220264@qq.com>
2020-04-29 00:20:33 +0800

    新增hbase协处理器

--- src/main/scala/com/ww/hbase/coprocessor/MyCoprocessor_1.java
+++ src/main/scala/com/ww/hbase/coprocessor/MyCoprocessor_1.java
@@ -3,14 +3,15 @@ package com.ww.hbase.coprocessor;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
-import org.apache.hadoop.hbase.ipc.RpcServer;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -29,6 +30,7 @@ public class MyCoprocessor_1 extends BaseRegionObserver {
         conf.set("hbase.zookeeper.quorum", "centos7-1:2181,centos7-2:2181,centos7-3:2181");
         try {
             conn = ConnectionFactory.createConnection(conf);
+            table = conn.getTable(TableName.valueOf("stu_tmp"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,13 +41,30 @@ public class MyCoprocessor_1 extends BaseRegionObserver {
                        Put put,
                        WALEdit edit,
                        Durability durability) throws IOException {
+        //获取当前表名
+        //String currentTableName = e.getEnvironment().getRegionInfo().getRegionNameAsString();
         byte[] row = put.getRow();
-        List<Cell> cells = put.get("cf".getBytes(), "star".getBytes());
+        List<Cell> cells = put.get("info".getBytes(), "name".getBytes());
         Cell cell = cells.get(0);
 
         Put new_put = new Put(row);
-        new_put.addColumn("cf".getBytes(),"fensi".getBytes(),cell.getValueArray());
+        new_put.addColumn("info".getBytes(),"name_copy".getBytes(),new String(cell.getValueArray(),cell.getValueOffset(),cell.getValueLength(),"UTF-8").getBytes());
         table.put(new_put);
         conn.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scan scan = new Scan();
+        ResultScanner scanner = table.getScanner(scan);
+        Iterator<Result> iterator = scanner.iterator();
+        while (iterator.hasNext()){
+            Result next = iterator.next();
+            List<Cell> columnCells = next.getColumnCells("info".getBytes(), "name".getBytes());
+            Cell cell = columnCells.get(0);
+            System.err.println(new String(cell.getValueArray(),cell.getValueOffset(),cell.getValueLength(),"UTF-8"));
+        }
+        table.close();
+        conn.close();
+
     }
 }
--- 笔记/Hbase/README.md
+++ 笔记/Hbase/README.md
@@ -5,3 +5,11 @@
 ```
 $HBASE_HOME/bin/hbase org.apache.hadoop.hbase.mapreduce.RowCounter 'tablename'
 ```
+
+### coprocessor
+
+```
+alter 'stu_tmp', METHOD => 'table_att', 'coprocessor' => \
+'hdfs://mycluster/app-jars/bigdata.jar|com.ww.hbase.coprocessor.MyCoprocessor_1|1001|'
+```
+
