2cee439b5ac77caf42ce10f0b4bcccd5ab741090
wangwei <530220264@qq.com>
2020-04-30 21:51:39 +0800

    增加Unsafe 增加ReentrantLock

---
+++ src/main/scala/com/ww/juc/JUC_Unsafe.java
@@ -0,0 +1,111 @@
+package com.ww.juc;
+
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author wangwei@huixiangtech.cn
+ * @version 1.0
+ * @className JUC_Unsafe
+ * @description TODO
+ * @date 2020/4/30-13:49
+ **/
+public class JUC_Unsafe {
+    private static Unsafe unsafe;
+    private static long valueOffset1;
+    private volatile static int value1;
+    private int value2;
+    private static int value3;
+    private static long valueOffset2;
+    private static int threadSize = 100;
+
+    static {
+        Field theUnsafe;
+        try {
+            theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            unsafe = (Unsafe) theUnsafe.get(null);
+            //  获取静态字段偏移量
+            valueOffset1 = unsafe.staticFieldOffset(JUC_Unsafe.class.getDeclaredField("value1"));
+            System.err.println(valueOffset1);
+            //  获取实例字段偏移量
+            long valueOffset2 = unsafe.objectFieldOffset(JUC_Unsafe.class.getDeclaredField("value2"));
+            System.err.println(valueOffset2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+        tryLock();
+    }
+
+    public static void tryLock() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1000);
+        for (int i = 0; i < 1000; i++) {
+            new Thread(() -> {
+                lock();
+                countDownLatch.countDown();
+            }).start();
+        }
+        countDownLatch.await();
+        System.err.println(value3);
+    }
+
+    public static void lock() {
+        unsafe.monitorEnter(JUC_Unsafe.class);
+        try {
+            value3++;
+        } finally {
+            unsafe.monitorExit(JUC_Unsafe.class);
+        }
+    }
+
+    public static void park_unpark() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            System.err.println(currentThreadName() + "--begin--" + System.currentTimeMillis());
+            unsafe.park(false, 0);
+            System.err.println(currentThreadName() + "----end--" + System.currentTimeMillis());
+        }, "Thread-1111");
+
+        Thread t2 = new Thread(() -> {
+            System.err.println(currentThreadName() + "--begin--" + System.currentTimeMillis());
+            //  在park之前调用unpark（多次调用效果同一次），再调用park时不会阻塞
+            unsafe.unpark(Thread.currentThread());
+            unsafe.unpark(Thread.currentThread());
+            unsafe.unpark(Thread.currentThread());
+            unsafe.park(false, TimeUnit.SECONDS.toNanos(3));
+            System.err.println(currentThreadName() + "----end--" + System.currentTimeMillis());
+        }, "Thread-2222");
+        t1.start();
+        t2.start();
+        TimeUnit.SECONDS.sleep(5);
+        unsafe.unpark(t1);
+    }
+
+    public static void test_add() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(threadSize);
+        for (int i = 0; i < threadSize; i++) {
+            new Thread(() -> {
+                add_1();
+                countDownLatch.countDown();
+            }).start();
+        }
+        countDownLatch.await();
+        System.err.println(Thread.currentThread().getName() + "--" + value1);
+    }
+
+    public static void add_1() {
+        int andAddInt = unsafe.getAndAddInt(JUC_Unsafe.class, valueOffset1, 1);
+        System.err.println(Thread.currentThread().getName() + "--" + andAddInt);
+    }
+
+    public static String currentThreadName() {
+        return Thread.currentThread().getName();
+    }
+}
---
+++ src/main/scala/com/ww/juc/Lock_ReentrantLock.java
@@ -0,0 +1,91 @@
+package com.ww.juc;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author wangwei@huixiangtech.cn
+ * @version 1.0
+ * @className Lock_ReentrantLock
+ * @description TODO
+ * @date 2020/4/30-11:41
+ **/
+public class Lock_ReentrantLock {
+
+    private static Unsafe unsafe;
+    static {
+        Field theUnsafe;
+        try {
+            theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            unsafe = (Unsafe) theUnsafe.get(null);
+            //  获取静态字段偏移量
+            //  获取实例字段偏移量
+            System.err.println(unsafe);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    private ReentrantLock lock = new ReentrantLock();
+
+    public void a() {
+        System.err.println(Thread.currentThread().getName() + "准备获取锁。。。" + System.currentTimeMillis());
+        lock.lock();
+        try {
+            System.err.println(Thread.currentThread().getName() + "获取到锁。。。" + System.currentTimeMillis());
+//            Thread.sleep(Integer.MAX_VALUE);
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+    public void b() {
+        System.err.println(Thread.currentThread().getName() + "准备获取锁。。。" + System.currentTimeMillis());
+        try {
+            if (lock.tryLock(5, TimeUnit.SECONDS)) {
+                try {
+                    System.err.println(Thread.currentThread().getName() + "获取到锁。。。" + System.currentTimeMillis());
+                    Thread.sleep(Integer.MAX_VALUE);
+                }finally {
+                    lock.unlock();
+                }
+            }else {
+                System.err.println(Thread.currentThread().getName() + "没有获取到锁。。。" + System.currentTimeMillis());
+            }
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void c() {
+        System.err.println(Thread.currentThread().getName() + "准备获取锁。。。" + System.currentTimeMillis());
+        lock.lock();
+        try {
+            System.err.println(Thread.currentThread().getName() + "获取到锁。。。" + System.currentTimeMillis());
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Lock_ReentrantLock t = new Lock_ReentrantLock();
+        Thread t1 = new Thread(() -> t.a());
+        t1.start();
+//        new Thread(() -> t.b()).start();
+        Thread t2 = new Thread(() -> t.c());
+        t2.start();
+        while (true){
+            TimeUnit.SECONDS.sleep(10);
+            System.err.println(111111111);
+//            unsafe.unpark(t2);
+        }
+    }
+}
