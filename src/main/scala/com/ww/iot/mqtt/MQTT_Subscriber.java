02c1704f5b5682bb66f81196f2b048cddcbb8438
wangwei <530220264@qq.com>
2020-05-09 20:59:36 +0800

    增加MQTT DEMO 字符串算法题

---
+++ src/main/scala/com/ww/iot/mqtt/MQTT_Subscriber.java
@@ -0,0 +1,65 @@
+package com.ww.iot.mqtt;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.text.MessageFormat;
+
+/**
+ * @author wangwei@huixiangtech.cn
+ * @version 1.0
+ * @className MQTT_Subscriber
+ * @description TODO
+ * @date 2020/5/9-16:59
+ **/
+
+import java.text.MessageFormat;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MQTT_Subscriber {
+    public static void main(String[] args) {
+        String broker = "tcp://120.24.103.229:1883";
+        String clientId = "Subscriber";
+        //Use the memory persistence
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker:" + broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+
+            String topic = "demo/topics";
+            System.out.println("Subscribe to topic:" + topic);
+            sampleClient.subscribe(topic);
+            sampleClient.setCallback(new MqttCallback() {
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    String theMsg = MessageFormat.format("{0} is arrived for topic {1}.", new String(message.getPayload()), topic);
+                    System.err.println(theMsg);
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                }
+
+                public void connectionLost(Throwable throwable) {
+                }
+            });
+        } catch (MqttException me) {
+            System.out.println("reason" + me.getReasonCode());
+            System.out.println("msg" + me.getMessage());
+            System.out.println("loc" + me.getLocalizedMessage());
+            System.out.println("cause" + me.getCause());
+            System.out.println("excep" + me);
+            me.printStackTrace();
+        }
+    }
+}
