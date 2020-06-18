75ac1ad5f930e6f850d02295293f9232d99f5fc3
wangwei <530220264@qq.com>
2020-06-18 19:25:58 +0800

    笔记

--- src/main/scala/com/ww/spark/streaming/NetworkWordCount.scala
+++ src/main/scala/com/ww/spark/streaming/NetworkWordCount.scala
@@ -4,16 +4,20 @@ import org.apache.spark.SparkConf
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+@SerialVersionUID(11111111111111111L)
 object NetworkWordCount {
   def main(args: Array[String]): Unit = {
-    if (args.length < 2) {
-      System.err.println("Usage: NetworkWordCount <hostname> <port>")
-      System.exit(1)
-    }
+//    if (args.length < 2) {
+//      System.err.println("Usage: NetworkWordCount <hostname> <port>")
+//      System.exit(1)
+//    }
 
-    val sparkConf = new SparkConf().setAppName("NetworkWordCount").setMaster("local[2]")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
-    val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_ONLY_SER)
+    val sparkConf = new SparkConf().setAppName("NetworkWordCount")
+      .setMaster("spark://wangwei:7077")
+//      .setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    ssc.sparkContext.addJar("E:\\workspace\\bigdata\\target\\sumEndPoint.jar")
+    val lines = ssc.socketTextStream("centos7-1", 9999.toInt, StorageLevel.MEMORY_ONLY_SER)
     val words = lines.flatMap(_.split(" "))
     val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
     wordCounts.print()
--- src/main/scala/com/ww/spark/streaming/SparkStreaming_Kafka.scala
+++ src/main/scala/com/ww/spark/streaming/SparkStreaming_Kafka.scala
@@ -1,80 +1,117 @@
 package com.ww.spark.streaming
 
-import java.sql.Timestamp
-import java.text.SimpleDateFormat
+import java.util.Properties
 
-import org.apache.spark.sql.SparkSession
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.{CanCommitOffsets, ConsumerStrategies, HasOffsetRanges, KafkaUtils, LocationStrategies}
+import org.apache.spark._
 
 object SparkStreaming_Kafka {
   def main(args: Array[String]): Unit = {
     val kafkaParams = Map[String, String](
       "bootstrap.servers" -> "centos7-1:9092",
       "group.id" -> "spark_streaming_3",
-      "auto.offset.reset" -> "earliest",
+      "auto.offset.reset" -> "latest",
       "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
-      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
+      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+      "auto.commit.enable" -> "false"
     )
-    val spark = SparkSession
-      .builder
-      .master("local[5]")
-      .appName("SparkStreaming_Kafka")
-      .getOrCreate()
-    val ssc = new StreamingContext(spark.sparkContext, Seconds(5))
-    ssc.sparkContext.setLogLevel("INFO")
-    ssc.checkpoint("../checkpoint/streaming_kafka_orders")
+    val ssc = new StreamingContext(new SparkConf()
+//      .setMaster("local[3]")
+            .setMaster("spark://wangwei:7077")
+      .setAppName("test-kafka"), Seconds(5))
+    ssc.sparkContext.setLogLevel("DEBUG")
+    ssc.sparkContext.addJar("E:\\\\workspace\\\\bigdata\\\\target\\\\sumEndPoint.jar")
 
-    val kafkaStream = KafkaUtils.createDirectStream[String, String](
-      ssc,
+    def producerConfiguration: Properties = {
+      val props = new Properties()
+      props.put("bootstrap.servers", "centos7-1:9092")
+      props.put("value.serializer", classOf[StringSerializer].getName)
+      props.put("key.serializer", classOf[StringSerializer].getName)
+      props.put("acks", "all")
+      props
+    }
+
+    val kafkaStreams = KafkaUtils.createDirectStream(ssc,
       LocationStrategies.PreferConsistent,
-      ConsumerStrategies.Subscribe[String, String](Set("orders"), kafkaParams))
+      ConsumerStrategies.Subscribe[String, String](Set("test"), kafkaParams))
 
-    case class Order(time: Timestamp, orderId: Long, clientId: Long,
-                     symbol: String, amount: Int, price: Double, buy: Boolean)
-    val orders = kafkaStream.flatMap(line => {
-      val dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
-      val s = line.value().split(",")
-      System.err.println(Thread.currentThread().getName + "---" + line.value())
-      try {
-        assert(s(6) == "B" || s(6) == "S")
-        List(Order(new Timestamp(dateFormat.parse(s(0)).getTime), s(1).toLong,
-          s(2).toLong, s(3), s(4).toInt, s(5).toDouble, s(6) == "B"))
-      } catch {
-        case e: Throwable => System.err.println("Wrong line format (" + e + "): " + line.value())
-          List()
-      }
+    kafkaStreams.foreachRDD(rdd => {
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd.map(x=>(x.value(),1)).foreachPartition(p => {
+        val producer = new KafkaProducer[String, String](producerConfiguration)
+        p.foreach({ case (k, v) =>
+          producer.send(new ProducerRecord[String, String]("test1", null, k + "--->" + v))
+        })
+      })
+      kafkaStreams.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
     })
-    val numberType = orders.map(o => (o.buy, 1L)).reduceByKey(_ + _)
-    val buySellList = numberType.map(t => {
-      if (t._1) ("Buys", List(t._2.toString))
-      else ("Sells", List(t._2.toString))
-    })
-    val amountPerClient = orders.map(o => (o.orderId, o.amount * o.price))
-    val amountState = amountPerClient.updateStateByKey((vals, total: Option[Double]) => {
-      total match {
-        case Some(total) => Some(vals.sum + total)
-        case None => Some(vals.sum)
-      }
-    })
-    val top5Client = amountState.transform(_.sortBy(_._2, false).map(_._1)
-      .zipWithIndex().filter(_._2 < 5))
-    val top5List = top5Client.repartition(1)
-      .map(x => x._1.toString)
-      .glom()
-      .map(arr => ("TOP5CLIENTS", arr.toList))
-    val stocksPerWindow = orders.map(x => (x.symbol, x.amount))
-      .reduceByKeyAndWindow((a1, a2) => a1 + a2, Minutes(60))
-    val topStocks = stocksPerWindow.transform(_.sortBy(_._2, false).map(_._1)
-      .zipWithIndex().filter(_._2 < 5))
-      .repartition(1)
-      .map(x => x._1.toString)
-      .glom()
-      .map(arr => ("TOP5STOCKS", arr.toList))
-    val finalStream = buySellList.union(top5List).union(topStocks).print()
-
     ssc.start()
     ssc.awaitTermination()
+    //    val spark = SparkSession
+    //      .builder
+    //      .master("local[2]")
+    //      .appName("SparkStreaming_Kafka")
+    //      .getOrCreate()
+    //    val ssc = new StreamingContext(spark.sparkContext, Seconds(2))
+    //    ssc.sparkContext.setLogLevel("DEBUG")
+    //    ssc.sparkContext.addJar("E:\\workspace\\bigdata\\target\\sumEndPoint.jar")
+    //    ssc.checkpoint("../checkpoint/streaming_kafka_orders")
+    //    val lines = ssc.socketTextStream("centos7-1", 9999)
+    //    lines.print()
+    //    val kafkaStream = KafkaUtils.createDirectStream[String, String](
+    //      ssc,
+    //      LocationStrategies.PreferConsistent,
+    //      ConsumerStrategies.Subscribe[String, String](Set("orders"), kafkaParams))
+    //
+    //    case class Order(time: Timestamp, orderId: Long, clientId: Long,
+    //                     symbol: String, amount: Int, price: Double, buy: Boolean)
+    //    val orders = kafkaStream.flatMap(line => {
+    //      val dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+    //      val s = line.value().split(",")
+    //      System.err.println(Thread.currentThread().getName + "---" + line.value())
+    //      try {
+    //        assert(s(6) == "B" || s(6) == "S")
+    //        List(Order(new Timestamp(dateFormat.parse(s(0)).getTime), s(1).toLong,
+    //          s(2).toLong, s(3), s(4).toInt, s(5).toDouble, s(6) == "B"))
+    //      } catch {
+    //        case e: Throwable => System.err.println("Wrong line format (" + e + "): " + line.value())
+    //          List()
+    //      }
+    //    })
+    //    val numberType = orders.map(o => (o.buy, 1L)).reduceByKey(_ + _)
+    //    val buySellList = numberType.map(t => {
+    //      if (t._1) ("Buys", List(t._2.toString))
+    //      else ("Sells", List(t._2.toString))
+    //    })
+    //    val amountPerClient = orders.map(o => (o.orderId, o.amount * o.price))
+    //    val amountState = amountPerClient.updateStateByKey((vals, total: Option[Double]) => {
+    //      total match {
+    //        case Some(total) => Some(vals.sum + total)
+    //        case None => Some(vals.sum)
+    //      }
+    //    })
+    //    val top5Client = amountState.transform(_.sortBy(_._2, false).map(_._1)
+    //      .zipWithIndex().filter(_._2 < 5))
+    //    val top5List = top5Client.repartition(1)
+    //      .map(x => x._1.toString)
+    //      .glom()
+    //      .map(arr => ("TOP5CLIENTS", arr.toList))
+    //    val stocksPerWindow = orders.map(x => (x.symbol, x.amount))
+    //      .reduceByKeyAndWindow((a1, a2) => a1 + a2, Minutes(60))
+    //    val topStocks = stocksPerWindow.transform(_.sortBy(_._2, false).map(_._1)
+    //      .zipWithIndex().filter(_._2 < 5))
+    //      .repartition(1)
+    //      .map(x => x._1.toString)
+    //      .glom()
+    //      .map(arr => ("TOP5STOCKS", arr.toList))
+    //    val finalStream = buySellList.union(top5List).union(topStocks).print()
+
+    //      ssc.start()
+    //    ssc.awaitTermination()
   }
 
 }
